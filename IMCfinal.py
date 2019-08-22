67ac05061808b16397f4759435c243cfd2500d38
Mariane Silva <54045364+mariaraujo815@users.noreply.github.com>
2019-08-22 14:44:29 -0300

    IMC Final com estrutura de decisão

---
+++ IMCfinal.py
@@ -0,0 +1,18 @@
+nome = str(input('Digite seu nome: '))
+peso = float(input('Digite seu peso: '))
+altura = float(input('Digite sua altura: '))
+imc = peso/(altura**2) 
+if (imc<17):
+    print("seu IMC é {:.2f} e Você está muito abaixo do peso!".format(imc))
+
+elif (imc>17 and imc<=18.5):
+    print("seu IMC é {:.2f} e  Você está abaixo do peso!".format(imc))
+
+elif (imc>18.5 and imc<=25.0):
+    print("seu IMC é {:.2f} e  Você está dentro da faixa de peso considerada normal pelo IMC!".format(imc))
+
+elif (imc>25.0 and imc<=30):
+    print("seu IMC é {:.2f} e  Você está acima do peso!".format(imc))
+
+else: 
+    print("seu IMC é {:.2f} e  Você está obeso!".format(imc))
