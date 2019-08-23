d5c5c8730b0e810d0395812b197897db449f1ef7
nataliasouza1306 <54067343+nataliasouza1306@users.noreply.github.com>
2019-08-22 22:08:45 -0300

    Update and rename imcinput.py to imcaddinput.py

---
+++ imcaddinput.py
@@ -0,0 +1,9 @@
+
+peso = float (input("Digite o seu peso: "))
+altura = float (input("Digite sua altura: "))
+
+imc = (peso/(altura**2))
+
+print("O peso informado foi: %s, e a altura informada foi: %s" %(peso, altura))
+print("Portanto, o seu IMC é: ", imc)
+
